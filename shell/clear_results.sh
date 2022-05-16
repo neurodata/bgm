7fb0f2de4c0e636d442858f9d7c0a67271caa89e
bdpedigo <benjamindpedigo@gmail.com>
2022-05-16 16:49:36 -0400

    clear results script

--- shell/clear_results.sh
+++ shell/clear_results.sh
@@ -1,3 +1,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BASE_DIR=$SCRIPT_DIR/..
 
+rm $BASE_DIR/results/glued_variables.json
+echo {}> $BASE_DIR/results/glued_variables.json
+
+rm $BASE_DIR/results/glued_variables.txt
