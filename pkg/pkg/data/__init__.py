2c6c2bbf04fb124539f14a0f6d62a7a3102683bc
bdpedigo <benjamindpedigo@gmail.com>
2022-05-19 10:46:52 -0400

    clean up data folder

--- pkg/pkg/data/__init__.py
+++ pkg/pkg/data/__init__.py
@@ -1,18 +1,1 @@
-from .load_data import (
-    DATA_PATH,
-    DATA_VERSION,
-    load_adjacency,
-    load_data,
-    load_edgelist,
-    load_maggot_graph,
-    load_matched,
-    load_navis_neurons,
-    load_network_palette,
-    load_networkx,
-    load_node_meta,
-    load_node_palette,
-    load_palette,
-    load_unmatched,
-    load_split_connectome,
-)
-from .preprocess import select_nice_nodes
+from .load_data import DATA_PATH, DATA_VERSION, load_split_connectome
